c00d7f55a2e9a5bd53cacdfc09cbdfb5a4ec91bf
Michael R Sweet <michaelrsweet@users.noreply.github.com>
2004-05-01 04:30:43 +0000

    Fix C++ class/struct handling.
    Move and separate test files for mxmldoc.

--- dotest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm -f testfile.xml
-valgrind --logfile-fd=3 --leak-check=yes ./mxmldoc testfile.xml testfile.cxx >testfile.html 2>testfile.log 3>testfile.valgrind
-
---
+++ test/class.cxx
@@ -0,0 +1,50 @@
+class foo_c			// Foo class
+{
+  float	foo;				/* Real number */
+  int	bar;				/* Integer */
+
+  public:
+
+  foo_c(float f, int b);
+  ~foo_c();
+
+  // 'get_bar()' - Get the value of bar.
+  int // O - Value of bar
+  get_bar()
+  {
+    return (bar);
+  }
+
+  // 'get_foo()' - Get the value of foo.
+  float // O - Value of foo
+  get_foo()
+  {
+    return (foo);
+  }
+
+  // 'set_bar()' - Set the value of bar.
+  void
+  set_bar(int b) // I - Value of bar
+  {
+    bar = b;
+  }
+
+  // 'set_foo()' - Set the value of foo.
+  void
+  set_foo(float f) // I - Value of foo
+  {
+    foo = f;
+  }
+}
+
+// 'foo_c::foo_c()' - Create a foo_c class.
+foo_c::foo_c(float f, int b)
+{
+  foo = f;
+  bar = b;
+}
+
+// 'foo_c::~foo_c()' - Destroy a foo_c class.
+foo_c::~foo_c()
+{
+}

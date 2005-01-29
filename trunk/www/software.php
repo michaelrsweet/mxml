ce2eadb6c8fba797bf34614b7490c8fb07cf68e0
mike <mike@52f9b3e4-c3ee-0310-a999-ee1bd40a8cf0>
2005-01-29 02:11:23 +0000

    Web site updates.
    comment.php:
        - Fix comment date.
    
    data/software.md5:
        - Moved from swfiles/mxml.md5 and reformatted.
    
    newsgroups.php:
        - Deleted
    
    phplib/html.php:
        - Removed newsgroups and polls links.
        - Renamed "support" to "bugs & features"
        - Add refresh URL support.
    
    software.php:
        - Use download mirrors.
        - Synced with other site code.
    
    str.php:
        - Change to "Bugs & Features"
        - Add 2.1.1 version number
    
    swfiles/*
        - Removed; all release files are on FTP server now.
        - mxml.md5 moved to data/software.md5

--- trunk/www/software.php
+++ trunk/www/software.php
@@ -1,6 +1,6 @@
 <?php
 //
-// "$Id: software.php,v 1.4 2004/05/19 22:45:23 mike Exp $"
+// "$Id$"
 //
 // Software download page.
 //
@@ -12,136 +12,194 @@
 include_once "phplib/html.php";
 
 
+// List of download servers...
+$sitelist = array(
+  "http://ftp.easysw.com/pub" => "Oregon, USA via HTTP",
+//  "ftp://ftp.easysw.com/pub" => "Oregon, USA via FTP",
+//  "http://ftp2.easysw.com/pub" => "Maryland, USA via HTTP",
+  "ftp://ftp2.easysw.com/pub" => "Maryland, USA via FTP",
+  "ftp://ftp3.easysw.com/pub" => "California, USA via FTP",
+  "http://ftp.funet.fi/pub/mirrors/ftp.easysw.com/pub" => "Espoo, Finland via HTTP",
+  "ftp://ftp.funet.fi/pub/mirrors/ftp.easysw.com/pub" => "Espoo, Finland via FTP",
+);
+
+
 // Get the list of software files...
-$dir   = opendir("swfiles");
+$fp    = fopen("data/software.md5", "r");
 $files = array();
 
-while ($file = readdir($dir))
-{
-  if (fnmatch("*.tar.gz", $file) ||
-      fnmatch("*.tar.bz2", $file))
-  {
-    // Add source file...
-    $files[$file] = substr($file, 5, strpos($file, ".tar") - 5);
-  }
-  else if (fnmatch("*.rpm", $file) ||
-	   fnmatch("*.deb", $file) ||
-	   fnmatch("*.tgz", $file))
-  {
-    // Add binary file...
-    $data = explode("-", $file);
-
-    $files[$file] = $data[1];
-  }
-}
-
-arsort($files);
-
-closedir($dir);
-
-// Read MD5 sums for each file...
-$fp  = fopen("swfiles/mxml.md5", "r");
-$md5 = array();
-
-while ($line = fgets($fp))
-{
-  $data          = explode(" ", trim($line));
-  $md5[$data[2]] = $data[0];
-}
-
-//print("<pre>md5 =\n");
-//print_r($md5);
-//print("</pre>\n");
+while ($line = fgets($fp, 255))
+  $files[sizeof($files)] = trim($line);
 
 fclose($fp);
 
-// Show files...
-html_header("Download");
-
-reset($files);
-
-if ($argc >= 1)
-  $firstversion = $argv[0];
+// Get form data, if any...
+if (array_key_exists("FILE", $_GET))
+  $file = $_GET["FILE"];
 else
-  $firstversion = current($files);
+  $file = "";
+
+if (array_key_exists("SITE", $_GET))
+{
+  $site = $_GET["SITE"];
+  setcookie("SITE", $site, time() + 90 * 86400, "/");
+}
+else if (array_key_exists("SITE", $_COOKIE) &&
+         array_key_exists($_COOKIE["SITE"], $sitelist))
+  $site = $_COOKIE["SITE"];
+else
+  $site = "";
+
+if (array_key_exists("VERSION", $_GET))
+  $version = $_GET["VERSION"];
+else
+{
+  $data    = explode(" ", $files[0]);
+  $version = $data[1];
+}
+
+// Show the standard header...
+if ($site != "" && $file != "")
+  html_header("Download", "", "$site/$file");
+else
+  html_header("Download");
 
 html_start_links(1);
-html_link("CVS", "#CVS");
 
 $curversion = "";
-while (list($file, $version) = each($files))
-  if ($version != $curversion)
+for ($i = 0; $i < sizeof($files); $i ++)
+{
+  // Grab the data for the current file...
+  $data     = explode(" ", $files[$i]);
+  $fversion = $data[1];
+
+  if ($fversion != $curversion)
   {
-    $curversion = $version;
-    html_link("v$version", "$PHP_SELF?$version");
+    $curversion = $fversion;
+    html_link("v$fversion", "$PHP_SELF?VERSION=$fversion");
   }
+}
+
+html_link("Subversion", "$PHP_SELF#SVN");
 
 html_end_links();
 
-print("<h1>Download</h1>");
+print("<h1>Download</h1>\n");
 
-html_start_table(array("Version", "Filename", "Size", "MD5 Sum"));
-
-reset($files);
-$curversion = "";
-while (list($file, $version) = each($files))
+// Show files or sites...
+if ($file != "")
 {
-  html_start_row();
-
-  if ($version == $firstversion)
-  {
-    $cs = "<th>";
-    $ce = "</th>";
-  }
+  if ($site != "")
+    print("<p>Your download should begin shortly. If not, please "
+         ."<a href='$site/$file'>click here</a> to download the file "
+	 ."from the current mirror.</p>\n"
+	 ."<h2>Change Mirror Site:</h2>\n");
   else
+    print("<p>Please select a mirror site below to begin the download.</p>\n"
+         ."<h2>Select Mirror Site:</h2>\n");
+
+  print("<form action='$PHP_SELF' method='GET' name='download'>\n"
+       ."<input type='hidden' name='FILE' value='"
+       . htmlspecialchars($file, ENT_QUOTES) . "'/>\n"
+       ."<input type='hidden' name='VERSION' value='"
+       . htmlspecialchars($version, ENT_QUOTES) . "'/>\n");
+
+  if ($site == "")
+    print("<input type='radio' name='SITE' value='' checked/>None<br />\n");
+
+  reset($sitelist);
+  while (list($key, $val) = each($sitelist))
   {
-    $cs = "<td align='center'>";
-    $ce = "</td>";
+    print("<input type='radio' name='SITE' value='$key' "
+         ."onClick='document.download.submit();'");
+    if ($site == $key)
+      print("  checked");
+    print("/>$val<br />\n");
   }
 
-  if ($version != $curversion)
+  if ($site != "")
+    print("<input type='submit' value='Change Mirror Site'/>\n");
+  else
+    print("<input type='submit' value='Select Mirror Site'/>\n");
+
+  print("</form>\n");
+}
+else
+{
+  // Show files...
+  print("<h2>Releases</h2>\n");
+
+  html_start_table(array("Version", "Filename", "Size", "MD5 Sum"));
+
+  $curversion = "";
+
+  for ($i = 0; $i < sizeof($files); $i ++)
   {
-    if ($curversion != "")
+    // Grab the data for the current file...
+    $data     = explode(" ", $files[$i]);
+    $md5      = $data[0];
+    $fversion = $data[1];
+    $filename = $data[2];
+    $basename = basename($filename);
+
+    html_start_row();
+
+    if ($fversion == $version)
     {
-      print("<td colspan='4'></td>");
-      html_end_row();
-      html_start_row();
+      $cs = "<th>";
+      $ce = "</th>";
+    }
+    else
+    {
+      $cs = "<td align='center'>";
+      $ce = "</td>";
     }
 
-    $curversion = $version;
-    print("$cs<a name='$version'>$version</a>$ce");
+    if ($fversion != $curversion)
+    {
+      if ($curversion != "")
+      {
+	print("<td colspan='4'></td>");
+	html_end_row();
+	html_start_row();
+      }
+
+      $curversion = $fversion;
+      print("$cs<a name='$fversion'>$fversion</a>$ce");
+    }
+    else
+      print("$cs$ce");
+
+    $kbytes = (int)((filesize("/home/ftp/pub/$filename") + 1023) / 1024);
+
+    print("$cs<a href='$PHP_SELF?VERSION=$version&amp;FILE=$filename'>"
+         ."<tt>$basename</tt></a>$ce"
+	 ."$cs${kbytes}k$ce"
+	 ."$cs<tt>$md5</tt>$ce");
+
+    html_end_row();
   }
-  else
-    print("$cs$ce");
 
-  $kbytes  = (int)((filesize("swfiles/$file") + 1023) / 1024);
-  $filemd5 = $md5["$file"];
+  html_end_table();
 
-  print("$cs<a href='swfiles/$file'><tt>$file</tt></a>$ce"
-       ."$cs${kbytes}k$ce"
-       ."$cs<tt>$filemd5</tt>$ce");
-
-  html_end_row();
+  print("<h2><a name='SVN'>Subversion Access</a></h2>\n"
+       ."<p>The $PROJECT_NAME software is available via Subversion "
+       ."using the following URL:</p>\n"
+       ."<pre>\n"
+       ."    <a href='http://svn.easysw.com/public/$PROJECT_MODULE/'>"
+       ."http://svn.easysw.com/public/$PROJECT_MODULE/</a>\n"
+       ."</pre>\n"
+       ."<p>The following command can be used to checkout the current "
+       ."$PROJECT_NAME source from Subversion:</p>\n"
+       ."<pre>\n"
+       ."    <kbd>svn co http://svn.easysw.com/public/$PROJECT_MODULE/trunk/ $PROJECT_MODULE</kbd>\n"
+       ."</pre>\n");
 }
 
-html_end_table();
-
-print("<h2><a name='CVS'>CVS Access</a></h2>\n"
-     ."<p>The $PROJECT_NAME software is available via anonymous CVS "
-     ."using the following CVS root:</p>\n"
-     ."<pre>\n"
-     ."    :pserver:anonymous@cvs.easysw.com:/home/anoncvs\n"
-     ."</pre>\n"
-     ."<p>The module name is <tt>$PROJECT_MODULE</tt>. The following "
-     ."command can be used to checkout the $PROJECT_NAME source from "
-     ."CVS:</p>\n"
-     ."<pre>\n"
-     ."    <kbd>cvs -d:pserver:anonymous@cvs.easysw.com:/home/anoncvs get $PROJECT_MODULE</kbd>\n"
-     ."</pre>\n");
-
+// Show the standard footer...
 html_footer();
 
 //
-// End of "$Id: software.php,v 1.4 2004/05/19 22:45:23 mike Exp $".
+// End of "$Id$".
 //
 ?>

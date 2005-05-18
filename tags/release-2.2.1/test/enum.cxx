ef98b7d2820b85f859abe64ab710bf683ae1d366
mike <mike@52f9b3e4-c3ee-0310-a999-ee1bd40a8cf0>
2005-05-18 01:48:04 +0000

    Remove bad release tag...

--- tags/release-2.2.1/test/enum.cxx
+++
@@ -1,7 +0,0 @@
-typedef enum foo_enum_e			/* Sample enumeration type */
-{
-  FOO_ONE,				/* One fish */
-  FOO_TWO,				/* Two fish */
-  FOO_RED,				/* Red fish */
-  FOO_BLUE				/* Blue fish */
-} foo_enum_t;

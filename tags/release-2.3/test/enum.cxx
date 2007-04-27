caed6287c22aa237339bab98f0b446944ea39e32
mike <mike@52f9b3e4-c3ee-0310-a999-ee1bd40a8cf0>
2007-04-27 01:50:51 +0000

    Tag 2.3

---
+++ tags/release-2.3/test/enum.cxx
@@ -0,0 +1,7 @@
+typedef enum foo_enum_e			/* Sample enumeration type */
+{
+  FOO_ONE,				/* One fish */
+  FOO_TWO,				/* Two fish */
+  FOO_RED,				/* Red fish */
+  FOO_BLUE				/* Blue fish */
+} foo_enum_t;

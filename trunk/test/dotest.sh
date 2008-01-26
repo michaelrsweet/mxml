36bcff7adcc13fadaa423d1e2565e357412f94d0
mike <mike@52f9b3e4-c3ee-0310-a999-ee1bd40a8cf0>
2008-01-26 20:50:28 +0000

    Bump version to 2.5.
    Add --heading option to place contact before table-of-contents and intro.
    
    Tweak the mxmldoc test suite.
    
    Use " instead of ' for attribute values.
    
    Quote titles, etc. as needed.
    
    Move TOC and HTML header generation to separate functions in preparation for
    frame support.

--- trunk/test/dotest.sh
+++ trunk/test/dotest.sh
@@ -1,14 +1,44 @@
 #!/bin/sh
 (cd ..; make mxmldoc-static)
 
-if test $# -gt 0; then
-	files=$*
-else
+files=""
+mode=""
+
+while test $# -gt 0; do
+	arg="$1"
+	shift
+
+	case "$arg" in
+		-g) mode="gdb" ;;
+		-v) mode="valgrind" ;;
+		*.h | *.c | *.cxx) files="$files $arg" ;;
+		*)
+			echo "Usage: ./dotest.sh [-g] [-v] [files]"
+			exit 1
+			;;
+	esac
+done
+
+if test "$files" = ""; then
 	files=*.cxx
 fi
 
 rm -f test.xml
-#../mxmldoc-static test.xml $files >test.html 2>test.log
-valgrind --log-fd=3 --leak-check=yes ../mxmldoc-static test.xml \
-	$files >test.html 2>test.log 3>test.valgrind
+
+case "$mode" in
+	gdb)
+		echo "run test.xml $files >test.html 2>test.log" >.gdbcmds
+		gdb -x .gdbcmds ../mxmldoc-static
+		;;
+
+	valgrind)
+		valgrind --log-fd=3 --leak-check=yes \
+			../mxmldoc-static test.xml $files \
+			>test.html 2>test.log 3>test.valgrind
+		;;
+
+	*)
+		../mxmldoc-static test.xml $files >test.html 2>test.log
+		;;
+esac
 

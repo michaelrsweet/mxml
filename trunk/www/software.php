a85cb0a8610041d04bc79a05e7ce23601b48e528
mike <mike@52f9b3e4-c3ee-0310-a999-ee1bd40a8cf0>
2008-08-21 16:43:16 +0000

    Drop nu6.org mirror.

--- trunk/www/software.php
+++ trunk/www/software.php
@@ -16,7 +16,6 @@ include_once "phplib/html.php";
 $sitelist = array(
   "ftp://ftp.easysw.com/pub" => "California, USA via FTP",
   "http://ftp.easysw.com/pub" => "California, USA via HTTP",
-  "http://www.nu6.org/_/mirror/ftp.easysw.com/pub" => "Gustavsberg, Sweden via HTTP",
   "http://ftp.funet.fi/pub/mirrors/ftp.easysw.com/pub" => "Espoo, Finland via HTTP",
   "ftp://ftp.funet.fi/pub/mirrors/ftp.easysw.com/pub" => "Espoo, Finland via FTP",
 );

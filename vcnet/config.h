4f00c46a3a4dfe94992d39b062f6fc339827232f
Michael Sweet <michael.r.sweet@gmail.com>
2017-03-29 20:08:15 -0400

    Sync up Visual Studio config.h with TOT.

--- vcnet/config.h
+++ vcnet/config.h
@@ -1,9 +1,7 @@
 /*
- * "$Id$"
+ * Configuration file for Mini-XML, a small XML file parsing library.
  *
- * Configuration file for Mini-XML, a small XML-like file parsing library.
- *
- * Copyright 2003-2016 by Michael R Sweet.
+ * Copyright 2003-2017 by Michael R Sweet.
  *
  * These coded instructions, statements, and computer programs are the
  * property of Michael R Sweet and are protected by Federal copyright
@@ -11,7 +9,7 @@
  * which should have been included with this file.  If this file is
  * missing or damaged, see the license at:
  *
- *     http://www.msweet.org/projects.php/Mini-XML
+ *     https://michaelrsweet.github.io/mxml
  */
 
 /*
@@ -64,7 +62,7 @@
  * Version number...
  */
 
-#define MXML_VERSION "Mini-XML v2.10"
+#define MXML_VERSION "Mini-XML v2.11"
 
 
 /*
@@ -82,10 +80,11 @@
 
 
 /*
- * Do we have the snprintf() and vsnprintf() functions?
+ * Do we have the *printf() functions?
  */
 
 #define HAVE_SNPRINTF 1
+/* #undef HAVE_VASPRINTF */
 #define HAVE_VSNPRINTF 1
 
 
@@ -94,6 +93,14 @@
  */
 
 #define HAVE_STRDUP 1
+/* #undef HAVE_STRLCPY */
+
+
+/*
+ * Do we have threading support?
+ */
+
+/* #undef HAVE_PTHREAD_H */
 
 
 /*
@@ -104,6 +111,11 @@
 extern char	*_mxml_strdup(const char *);
 #    define strdup _mxml_strdup
 #  endif /* !HAVE_STRDUP */
+
+#  ifndef HAVE_STRLCPY
+extern size_t	_mxml_strlcpy(char *, const char *, size_t);
+#    define strlcpy _mxml_strlcpy
+#  endif /* !HAVE_STRLCPY */
 
 extern char	*_mxml_strdupf(const char *, ...);
 extern char	*_mxml_vstrdupf(const char *, va_list);
@@ -117,7 +129,3 @@ extern int	_mxml_snprintf(char *, size_t, const char *, ...);
 extern int	_mxml_vsnprintf(char *, size_t, const char *, va_list);
 #    define vsnprintf _mxml_vsnprintf
 #  endif /* !HAVE_VSNPRINTF */
-
-/*
- * End of "$Id$".
- */

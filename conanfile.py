a62033f7acebc069ba70ad9711c48d44d4f88f0c
Lance Wallis <walance@pvas.me>
2024-06-17 18:34:00 -0500

    Working conan build.

--- conanfile.py
+++ conanfile.py
@@ -4,8 +4,24 @@ from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 class mxmlRecipe(ConanFile):
     name = "mxml"
     version = "4.0.4"
+    description = "Mini-XML - Tiny XML Parsing Library v4"
+    homepage = "https://github.com/michaelrsweet/mxml.git"
+    url = "https://github.com/michaelrsweet/mxml.git"
+    topics = ("xml")
+    package_type = "library"
 
     settings = "os", "compiler", "build_type", "arch"
+
+    options = { "shared": [True, False] }
+    default_options = { "shared": False }
+
+    @property
+    def _msbuild_configuration(self):
+        return "Debug" if self.settings.build_type == "Debug" else "Release"
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
 
     def layout(self):
         cmake_layout(self)

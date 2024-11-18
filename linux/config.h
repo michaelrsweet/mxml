880a46116019843ddc9032f3b109cd940b6af11d
Michael R Sweet <michael.r.sweet@gmail.com>
2024-11-18 09:29:28 -0500

    Add linux makefiles (Issue #335)

---
+++ linux/config.h
@@ -0,0 +1,43 @@
+/* config.h.  Generated from config.h.in by configure.  */
+//
+// Configuration file for Mini-XML, a small XML file parsing library.
+//
+// https://www.msweet.org/mxml
+//
+// Copyright © 2003-2024 by Michael R Sweet.
+//
+// Licensed under Apache License v2.0.  See the file "LICENSE" for more
+// information.
+//
+
+#ifndef MXML_CONFIG_H
+#  define MXML_CONFIG_H
+#  include <stdio.h>
+#  include <stdlib.h>
+#  include <string.h>
+#  include <stdarg.h>
+#  include <ctype.h>
+
+
+//
+// Version number
+//
+
+#  define MXML_VERSION "Mini-XML v4.0.4"
+
+
+//
+// Inline function support
+//
+
+#  define inline
+
+
+//
+// Have <pthread.h>?
+//
+
+#  define HAVE_PTHREAD_H 1
+
+
+#endif // !MXML_CONFIG_H
